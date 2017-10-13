0693649edd7eb41288c9fac7bd572a5f2065240a
jmelberg <jordan.melberg@okta.com>
2017-10-13 11:56:17 -0700

    Updated per rc review

--- _source/_assets/js/myOkta.js
+++ _source/_assets/js/myOkta.js
@@ -1,27 +1,32 @@
 (function($) {    
-  $(window).load(function() {
+  $(function() {
     // Once the page is loaded, find the hidden iframe element
     // and make a postMessage request
-    var iframe = document.getElementById('myOkta');
-    if (!iframe) { return; }
+
+    var iframe = $('<iframe id="myOktaIFrame" src="https://login.okta.com/discovery/iframe.html" style="display:none"></iframe>').get(0);
+
+    $('body').append(iframe);
+
+    $('#myOktaIFrame').load(function() {
+      iframe.contentWindow.postMessage({messageType: 'get_accounts_json'}, 'https://login.okta.com');
+    });
     window.addEventListener('message', receiveMessage, false);
-    iframe.contentWindow.postMessage({messageType: 'get_accounts_json'}, 'https://login.okta.com');
   });
 
   function receiveMessage(event) {
     // Verify the event origin is trusted
-    if (event.origin !== 'https://login.okta.com' && !event.data) {
+    if (event.origin !== 'https://login.okta.com' || !event.data) {
       return;
     }
 
     function findPreviewOrg(accounts) {
       // Extract the first 'oktapreview' domain
-      for (i=0; i< accounts.length; i++) {
+      for (i = 0; i < accounts.length; i++) {
         if (accounts[i].origin.indexOf('.oktapreview.com') !== -1){
           return accounts[i].origin;
         }
       }
-      return 'https://{yourOktaDomain}.com'
+      return 'https://{yourOktaDomain}.com';
     }
 
     var previewOrg = findPreviewOrg(event.data);
